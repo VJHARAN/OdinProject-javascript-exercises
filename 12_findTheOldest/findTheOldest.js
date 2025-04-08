14fd758b2e4cd464805688f8ebfe8527d3d64f75
VJHARAN <haranvj1997@gmail.com>
2025-04-08 20:11:09 +0530

    Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+     let d=new Date(2025,3);
+     let res=people.sort((person1,person2)=>{
+        if (!person1.yearOfDeath)
+            person1.yearOfDeath=d.getFullYear();
+        if (!person2.yearOfDeath)
+            person2.yearOfDeath=d.getFullYear();
+        
+        p1Age=person1.yearOfDeath-person1.yearOfBirth;
+        p2Age=person2.yearOfDeath-person2.yearOfBirth;
+        return p1Age>p2Age?-1:1;
+    });
+    return (res[0]);
+    
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
