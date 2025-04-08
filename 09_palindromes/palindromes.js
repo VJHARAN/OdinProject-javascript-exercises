2e8b0e0d50729aacd5cb01f6475c6f1392d147b0
VJHARAN <haranvj1997@gmail.com>
2025-04-08 16:44:28 +0530

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+   
+    const withoutPunctuations=sentence.replace(/\W/g,'');
+   
+    const letters=[...withoutPunctuations.toLowerCase()];
+ 
+    const copied=letters.slice();
+  
 
+    letters.reverse();
+
+
+    return (copied.every((value,index)=>value===letters[index]));
 };
-
 // Do not edit below this line
 module.exports = palindromes;
+ 
