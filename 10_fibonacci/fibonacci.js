df07bb79f77486419e460306cfe64773ca8b6ed3
VJHARAN <haranvj1997@gmail.com>
2025-04-08 19:18:51 +0530

    Fibonacci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(pos) {
+    const fib=[1];
+    pos=+pos;
+   
+    if (pos<0)
+        return "OOPS";
+    else if (pos===0)
+        return 0;
+    else if (pos===1 || pos===2)
+        return 1;
+    else
+        return fibonacci(pos-1)+fibonacci(pos-2);  
+    };
 
 // Do not edit below this line
 module.exports = fibonacci;
+ 
